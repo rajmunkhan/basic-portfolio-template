4d1a337a3502304077ce43e9873d6965fc55d2de
raaj <rajmunkhan963@gmail.com>
2021-10-18 15:51:03 +0530

    first comming of basic portfolio

---
+++ projects.js
@@ -0,0 +1,6 @@
+let projects=[
+    {projectName:"driver detector",projectDesc:"this is the first project and made up of html css"},
+    {projectName:"Till mark",projectDesc:"this is the ecommerce project and made up of html css php js"},
+    {projectName:"SRMforum",projectDesc:"a forum where students can share the events"},
+    {projectName:"Claroom",projectDesc:"Here teacher can assign the tast and students can submit the pdf"},
+]
