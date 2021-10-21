02347f8bef3f6ea46e22d8ee169ca5a763e54365
briangor <briangor13@gmail.com>
2021-10-21 06:34:50 -0400

    Added hamburger menu animation

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,7 @@ const navLinks = document.querySelector('.navbar-links');
 
 toggleBtn.addEventListener('click', function(){
     navLinks.classList.toggle("active");
+    toggleBtn.classList.toggle("open");
 })
 navLinks.addEventListener('click', function(){
     navLinks.classList.toggle("active");
